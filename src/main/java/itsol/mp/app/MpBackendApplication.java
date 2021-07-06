c1af7bfbdbc62c29f73d90660364702b954fbb88
tunglam20895 <tunglam20895@gmail.com>
2021-07-06 13:13:35 +0700

    Initial commit

---
+++ src/main/java/itsol/mp/app/MpBackendApplication.java
@@ -0,0 +1,13 @@
+package itsol.mp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpBackendApplication.class, args);
+
+	}
+}
---
+++ src/test/java/itsol/mp/app/MpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package itsol.mp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
