3e07209a765490dbfca88d099249478cbdd5e3f9
tunglam20895 <tunglam20895@gmail.com>
2021-07-17 13:19:01 +0700

    add public controller

---
+++ src/main/java/itsol/mp/app/controllers/PublicController.java
@@ -0,0 +1,69 @@
+package itsol.mp.app.controllers;
+
+import itsol.mp.app.entities.Users;
+import itsol.mp.app.services.UserService;
+import itsol.mp.app.utils.CustomErrorType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.Random;
+
+@RestController
+@RequestMapping("/public")
+public class PublicController {
+
+    public static final Logger logger = LoggerFactory.getLogger(PublicController.class);
+
+    Random OTP = new Random();
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/login")
+    public Principal login(Principal principal) {
+        logger.info("user logged " + principal);
+        return principal;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody Users newUser) {
+        if (userService.findUserByUsername(newUser.getUsername()) != null) {
+            logger.error("username Already exist " + newUser.getUsername());
+            return new ResponseEntity(
+                    new CustomErrorType("user with username " + newUser.getUsername() + "already exist "),
+                    HttpStatus.CONFLICT);
+        }
+        newUser.setRole("ROLE_MEMBER");
+        newUser.setPassword(encoder.encode(newUser.getPassword()));
+        return new ResponseEntity<Users>(userService.addUser(newUser), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/sendOTP")
+    public int send(@RequestBody String email) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        System.out.println("SAO THẾ??????"+email);
+        int otp = OTP.nextInt(9999 - 1000);
+        msg.setTo(email);
+        msg.setSubject("Xác NHẬN ĐĂNG KÝ");
+        msg.setText("Mã OTP của bạn là " +otp + " vui lòng không chia sẻ mã với bất cứ ai để đảm bảo bảo mật thông tin của bạn!!!");
+        javaMailSender.send(msg);
+        return otp;
+    }
+
+}
---
+++ src/main/java/itsol/mp/app/services/UserService.java
@@ -0,0 +1,34 @@
+package itsol.mp.app.services;
+
+import itsol.mp.app.entities.Users;
+import itsol.mp.app.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Users addUser(Users users) {
+        return userRepository.save(users);
+    }
+
+    public List<Users> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Users updateUser(Users users) {
+        return userRepository.save(users);
+    }
+
+    public Users findUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+}
